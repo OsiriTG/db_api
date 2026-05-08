6a9f69d0b38e5266def728b90bdea75c4032e066
ibrvtk <nicecock@disroot.org>
2026-05-08 11:15:13 +0300

    schema.sql: Полное копирование Телеграма
    - Таблица users разделена на users и chats.
    - Добавлены все действующие параметры agioram.types User и Chat.

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -8,20 +8,46 @@ END $$;
 
 CREATE TABLE IF NOT EXISTS users (
     id BIGINT PRIMARY KEY,
-    is_bot BOOLEAN NOT NULL DEFAULT FALSE,
+    is_bot BOOLEAN NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT DEFAULT NULL,
+    username TEXT DEFAULT NULL,
+    language_code TEXT DEFAULT NULL,
+    is_premium BOOLEAN DEFAULT NULL,
+    added_to_attachment_menu BOOLEAN DEFAULT NULL,
+    can_join_groups BOOLEAN DEFAULT NULL,
+    can_read_all_group_messages BOOLEAN DEFAULT NULL,
+    supports_inline_queries BOOLEAN DEFAULT NULL,
+    can_connect_to_business BOOLEAN DEFAULT NULL,
+    has_main_web_app BOOLEAN DEFAULT NULL,
+    has_topics_enabled BOOLEAN DEFAULT NULL,
+    allows_users_to_create_topics BOOLEAN DEFAULT NULL,
+    can_manage_bots BOOLEAN DEFAULT NULL,
+
+    "oid" TEXT NOT NULL,
+    zoneinfo TEXT DEFAULT NULL,
+    date_db TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+    UNIQUE(username, "oid")
+);
+
+CREATE TABLE IF NOT EXISTS chats (
+    id BIGINT PRIMARY KEY,
     "type" chat_type NOT NULL,
-    title VARCHAR(128) DEFAULT NULL,
-    first_name VARCHAR(128) DEFAULT NULL,
-    last_name VARCHAR(128) DEFAULT NULL,
-    username VARCHAR(32) DEFAULT NULL,
-    language_code VARCHAR(2) DEFAULT NULL,
-    is_premium BOOLEAN NOT NULL DEFAULT FALSE,
-    shifted_id INTEGER NOT NULL,
+    title TEXT DEFAULT NULL,
+    username TEXT DEFAULT NULL,
+    first_name TEXT DEFAULT NULL,
+    last_name TEXT DEFAULT NULL,
+    is_forum BOOLEAN DEFAULT NULL,
+    is_direct_messages BOOLEAN DEFAULT NULL,
+
+    language_code TEXT DEFAULT NULL,
     "oid" TEXT NOT NULL,
     owner_id BIGINT REFERENCES users(id) ON DELETE SET NULL DEFAULT NULL,
     zoneinfo TEXT DEFAULT NULL,
-    date_reg_db TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    UNIQUE(username, shifted_id, "oid")
+    date_db TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+    UNIQUE(username, "oid")
 );
 
 CREATE TABLE IF NOT EXISTS api_keys (
@@ -29,13 +55,15 @@ CREATE TABLE IF NOT EXISTS api_keys (
     permissions VARCHAR(4) NOT NULL DEFAULT '-r--',
     can_create_api_keys BOOLEAN NOT NULL DEFAULT FALSE,
     owner_id BIGINT REFERENCES users(id) ON DELETE SET NULL DEFAULT NULL,
-    date_reg TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    date_db TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX IF NOT EXISTS idx_users_username ON users(username);
-CREATE INDEX IF NOT EXISTS idx_users_shifted_id ON users(shifted_id);
 CREATE INDEX IF NOT EXISTS idx_users_oid ON users("oid");
-CREATE INDEX IF NOT EXISTS idx_users_owner_id ON users(owner_id);
+
+CREATE INDEX IF NOT EXISTS idx_chats_username ON chats(username);
+CREATE INDEX IF NOT EXISTS idx_chats_owner_id ON chats(owner_id);
+
 CREATE INDEX IF NOT EXISTS idx_api_keys_owner_id ON api_keys(owner_id);
 
 COMMIT;
